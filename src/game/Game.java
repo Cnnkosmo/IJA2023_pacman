67dcabe2e7f0c9acc9384be1fbabc6c92ac8698a
Cnnkosmo <hristoigor920@gmail.com>
2023-05-02 17:23:53 +0200

    added comments

--- src/game/Game.java
+++ src/game/Game.java
@@ -3,7 +3,9 @@ package game;
 import common.Tile;
 
 import java.util.ArrayList;
-
+/*
+*main class which will allow us to run our game and where is rendering of the map happening
+ */
 public class Game {
     public static void main(String[] args) {
         Maze_cfg cfg = new Maze_cfg();
@@ -22,6 +24,10 @@ public class Game {
         view.render(pacman,ghosts,map);
 //        System.out.println(view.samePosGhost(ghosts, map.getTile(2,3)));
     }
+    /*
+    *@brief return created pacman with the row and col values setted for same values as the tile with it start position
+    *@param takes the whole map to look through and find start position for pacman
+     */
     public static Pacman createPacman(MazeMap map) {
 
         for (Tile[] row : map.map) {
@@ -35,6 +41,10 @@ public class Game {
         }
         return null;
     }
+    /*
+    *@brief method returns array of ghosts same principle as for createPacman method
+     *@param map
+     */
     public static ArrayList<Ghost> createGhosts(MazeMap map) {
         ArrayList<Ghost> ghosts = new ArrayList<Ghost>();
         for (Tile[] row : map.map) {
@@ -48,6 +58,11 @@ public class Game {
         }
         return ghosts;
     }
+    /*
+    *@brief method that returns a list of controllers for ghosts(because we have unlimited number of ghosts so unlimited
+    * number of controllers as well)
+    * @param List of ghosts, map to set a reference to the whole map to the every controller
+     */
     public static ArrayList<GhostController> setGhostControllers(ArrayList<Ghost> ghosts, MazeMap map)
     {
         ArrayList<GhostController> ghostCntrl = new ArrayList<GhostController>();
