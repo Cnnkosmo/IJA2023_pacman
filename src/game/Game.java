4410d3e092dadcc9f8ab81af8a694f877246f314
Cnnkosmo <hristoigor920@gmail.com>
2023-05-02 15:21:22 +0200

    Reworked setters for Ghost Contorllers, and pacman controller

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,72 +5,56 @@ import common.Tile;
 import java.util.ArrayList;
 
 public class Game {
-
-    static PacmanController pacmanController;
-    static ArrayList<GhostController> ghostControllers = new ArrayList<GhostController>();
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Maze_cfg cfg = new Maze_cfg();
-        cfg.startReading(2,3);
+        cfg.startReading(3, 3);
         cfg.processLine("X.S");
         cfg.processLine(".GG");
+        cfg.processLine(".GG");
         cfg.stopReading();
-        MazeMap map =  cfg.createMaze();
-        setControllers(map);
-        printBoard(map);
+        MazeMap map = cfg.createMaze();
+        Pacman pacman = createPacman(map);
+        ArrayList<Ghost> ghosts = createGhosts(map);
+        PacmanController pacmanController = new PacmanController(pacman,map);
+        ArrayList<GhostController> ghostControllers = setGhostControllers(ghosts,map);
         pacmanController.update(Tile.Direction.L);
-        printBoard(map);
     }
 
-    public static void setControllers(MazeMap map){
-        for (Tile[] row : map.map)
-        {
-            for (Tile item : row)
-            {
-                if (item instanceof Path)
-                {
-                    if (((Path)item).isStartPac())
-                    {
-                        pacmanController = new PacmanController(new Pacman(item.getRow(),item.getColumn()),map);
-                    }
-                    else if (((Path)item).isStartGhost())
-                    {
-                        ghostControllers.add(new GhostController(new Ghost(item.getRow(),item.getColumn()),map));
+    public static Pacman createPacman(MazeMap map) {
+
+        for (Tile[] row : map.map) {
+            for (Tile item : row) {
+                if (item instanceof Path) {
+                    if (((Path) item).isStartPac()) {
+                        return new Pacman(item.getRow(), item.getColumn());
                     }
                 }
             }
         }
+        return null;
     }
 
-
-    public static void printBoard(MazeMap map){
-        for (Tile[] row : map.map)
-        {
-            for (Tile item : row)
-            {
-                if (item instanceof Path)
-                {
-                    if (((Path) item).isStartGhost())
-                    {
-                        System.out.print("G" + " ");
+    public static ArrayList<Ghost> createGhosts(MazeMap map) {
+        ArrayList<Ghost> ghosts = new ArrayList<Ghost>();
+        for (Tile[] row : map.map) {
+            for (Tile item : row) {
+                if (item instanceof Path) {
+                    if (((Path) item).isStartGhost()) {
+                        ghosts.add(new Ghost(item.getRow(), item.getColumn()));
                     }
-                    else if (((Path) item).isStartPac())
-                    {
-                        System.out.print("S" + " ");
-                    }
-                    else
-                    {
-                        System.out.print("." + " ");
-                    }
-                }
-                else if (item instanceof Wall)
-                {
-                    System.out.print("X" + " ");
                 }
             }
-            System.out.println('\n');
         }
-        System.out.println("COLS "+ map.numCols());
-        System.out.println("ROWS "+ map.numRows());
+        return ghosts;
     }
+    public static ArrayList<GhostController> setGhostControllers(ArrayList<Ghost> ghosts, MazeMap map)
+    {
+        ArrayList<GhostController> ghostCntrl = new ArrayList<GhostController>();
+        for (int i = 0; i < ghosts.size(); i++)
+        {
+            ghostCntrl.add(new GhostController((ghosts.get(i)), map));
+        }
+        return ghostCntrl;
+    }
+
 }
