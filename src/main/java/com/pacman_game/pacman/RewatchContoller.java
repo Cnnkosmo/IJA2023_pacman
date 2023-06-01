da38a20ce9a615dd457e69a0dec9295406769082
rtk <hristoigor920@gmail.com>
2023-06-01 12:48:08 +0200

    fixed out of bounds problem in log array

--- src/main/java/com/pacman_game/pacman/RewatchContoller.java
+++ src/main/java/com/pacman_game/pacman/RewatchContoller.java
@@ -11,13 +11,22 @@ public class RewatchContoller
     public void renderNext()
     {
         rewatchUi.setIndex(rewatchUi.getIndex()+1);
-        rewatchUi.getRoot(rewatchUi.maps.get(rewatchUi.getIndex()));
+        System.out.println("current ind" + rewatchUi.getIndex());
+        System.out.println("maps.size" + rewatchUi.maps.size());
+        if (rewatchUi.getIndex() < rewatchUi.maps.size())
+        {
+            rewatchUi.getRoot(rewatchUi.maps.get(rewatchUi.getIndex()));
+        }
         System.out.println("E pushed");
+
     }
     public void renderPrevious()
     {
-        rewatchUi.setIndex(rewatchUi.getIndex()-1);
-        rewatchUi.getRoot(rewatchUi.maps.get(rewatchUi.getIndex()));
-        System.out.println("Q pushed");
+            rewatchUi.setIndex(rewatchUi.getIndex()-1);
+            if (rewatchUi.getIndex() > 0 && rewatchUi.getIndex() < rewatchUi.maps.size())
+            {
+                rewatchUi.getRoot(rewatchUi.maps.get(rewatchUi.getIndex()));
+            }
+            System.out.println("Q pushed");
     }
 }
